eea5a650874b44c6eaa2b35b8d261038a0e9bb96
sagarje6894 <sagargarje0077@gmail.com>
2023-12-30 19:36:21 +0530

    updations and executable files

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -23,10 +23,10 @@ from src.utils import save_object,evaluate_models
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
 
+
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
@@ -87,10 +87,10 @@ class ModelTrainer:
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models,param=params)
             
-            ## To get best model score from dict
+            # To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
-            ## To get best model name from dict
+            # To get best model name from dict
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
@@ -110,10 +110,6 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
-            
 
-
-
-            
         except Exception as e:
             raise CustomException(e,sys)
